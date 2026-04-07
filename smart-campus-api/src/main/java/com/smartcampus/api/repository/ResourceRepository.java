5611b2d25371bed56b2a4a63c2acee89fb533c6a
Dismi Liyanage <it23346154@my.sliit.lk>
2026-04-07 11:18:18 +0530

    feat: implement ResourceRepository

---
+++ smart-campus-api/src/main/java/com/smartcampus/api/repository/ResourceRepository.java
@@ -0,0 +1,9 @@
+package com.smartcampus.api.repository;
+
+import com.smartcampus.api.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+}
