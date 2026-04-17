68f0d0f2274ccd104899470ec32a350f3282caf5
Dismi Liyanage <it23346154@my.sliit.lk>
2026-04-17 11:59:35 +0530

    feat: add AmenityDTO.java & AssetDTO.java

--- smart-campus-api/src/main/java/com/smartcampus/api/dto/AssetDTO.java
+++ smart-campus-api/src/main/java/com/smartcampus/api/dto/AssetDTO.java
@@ -1,5 +1,6 @@
 package com.smartcampus.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AssetDTO {
     private Long id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
     private String description;
 }
